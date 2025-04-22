1f9e6e32c36b539f158d8f40c4e515d4926c7694
Kolexas <maksimilyan.klimentev39@gmail.ru>
2025-04-22 15:18:21 +0300

    Control of DB schemes: foreign key moved to person.

--- school/src/scripts422.sql
+++ school/src/scripts422.sql
@@ -1,14 +1,14 @@
-Create TABLE Owner (
- owner_id SERIAL Primary KEY,
- age INTEGER,
- name TEXT,
- driver_license BOOLEAN
- );
-
  Create TABLE car (
- owner_id INTEGER REFERENCES Owner(owner_id),
  car_id SERIAL Primary KEY,
  car_brand TEXT,
  model TEXT,
  cost INTEGER
  );
+
+ Create TABLE Owner (
+  owner_id SERIAL Primary KEY,
+  car_id INTEGER REFERENCES car(car_id),
+  age INTEGER,
+  name TEXT,
+  driver_license BOOLEAN
+  );
